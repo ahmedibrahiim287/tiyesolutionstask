77ce71255077a60f94ed88ad5a90ee00d35b2746
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-05-08 23:12:50 +0200

    22

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,6 +3,13 @@ jQuery( document ).ready(function( $ ) {
 
 	"use strict";
 
+
+    
+        $(function() {
+            $( "#tabs" ).tabs();
+        });
+
+
         // Page loading animation
 
         $("#preloader").animate({
@@ -25,9 +32,8 @@ jQuery( document ).ready(function( $ ) {
             $("header").removeClass("background-header");
           }
         });
-        
-        if ($('.owl-clients').length) {
-            $('.owl-clients').owlCarousel({
+		if ($('.owl-testimonials').length) {
+            $('.owl-testimonials').owlCarousel({
                 loop: true,
                 nav: false,
                 dots: true,
@@ -50,20 +56,19 @@ jQuery( document ).ready(function( $ ) {
                         margin: 20
                     },
                     992: {
-                        items: 3,
+                        items: 2,
                         margin: 30
                     }
                 }
             });
         }
-
-        if ($('.owl-banner').length) {
-            $('.owl-banner').owlCarousel({
+        if ($('.owl-partners').length) {
+            $('.owl-partners').owlCarousel({
                 loop: true,
                 nav: false,
                 dots: true,
                 items: 1,
-                margin: 0,
+                margin: 30,
                 autoplay: false,
                 smartSpeed: 700,
                 autoplayTimeout: 6000,
@@ -77,14 +82,65 @@ jQuery( document ).ready(function( $ ) {
                         margin: 0
                     },
                     576: {
-                        items: 1,
+                        items: 2,
                         margin: 20
                     },
                     992: {
-                        items: 1,
+                        items: 4,
                         margin: 30
                     }
                 }
             });
         }
+
+        $(".Modern-Slider").slick({
+            autoplay:true,
+            autoplaySpeed:10000,
+            speed:600,
+            slidesToShow:1,
+            slidesToScroll:1,
+            pauseOnHover:false,
+            dots:true,
+            pauseOnDotsHover:true,
+            cssEase:'linear',
+           // fade:true,
+            draggable:false,
+            prevArrow:'<button class="PrevArrow"></button>',
+            nextArrow:'<button class="NextArrow"></button>', 
+        });
+
+        function visible(partial) {
+            var $t = partial,
+                $w = jQuery(window),
+                viewTop = $w.scrollTop(),
+                viewBottom = viewTop + $w.height(),
+                _top = $t.offset().top,
+                _bottom = _top + $t.height(),
+                compareTop = partial === true ? _bottom : _top,
+                compareBottom = partial === true ? _top : _bottom;
+
+            return ((compareBottom <= viewBottom) && (compareTop >= viewTop) && $t.is(':visible'));
+
+        }
+
+        $(window).scroll(function(){
+
+          if(visible($('.count-digit')))
+            {
+              if($('.count-digit').hasClass('counter-loaded')) return;
+              $('.count-digit').addClass('counter-loaded');
+              
+        $('.count-digit').each(function () {
+          var $this = $(this);
+          jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+            duration: 3000,
+            easing: 'swing',
+            step: function () {
+              $this.text(Math.ceil(this.Counter));
+            }
+          });
+        });
+        }
+    })
+ 
 });
